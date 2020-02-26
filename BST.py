3971aa9521b79030e929e1a593e8af1c8e925f06
IGuessThisIsMyLife <47003641+IGuessThisIsMyLife@users.noreply.github.com>
2020-02-25 23:35:20 -0500

    Update BST.py

--- BST.py
+++ BST.py
@@ -1,5 +1,5 @@
-#Jonathan Nunes
-#Project 1: Trees
+# Jonathan Nunes
+# Project 1: Trees
 
 import random
 
@@ -8,7 +8,8 @@ class Node:
         """Node Constructor"""
         self.left = None
         self.right = None
-        self.data = n    
+        self.data = n
+
 
 def insertRec(root, node):
     """Recursive Insertion"""
@@ -25,18 +26,34 @@ def insertRec(root, node):
                 root.left = node
             else:
                 insertRec(root.left, node)
-        
-def deleteRec(root, node):
+
+
+def deleteRec(root, n):
     """Recursive Deletion"""
-    pass
+    
 
-def findNextRec(root, node):
+
+def findNextRec(root, n):
     """Recursive Find Next"""
-    pass
+    if root.data < n:
+        print("Recursively Found Next: %i" % root.data)
+        return
+    if root.left is not None:
+        findNextRec(root.left, n)
+    if root.right is not None:
+        findNextRec(root.right, n)
 
-def findPrevRec(root, node):
+
+def findPrevRec(root, n):
     """Recursive Find Prev"""
-    pass
+    if root.data > n:
+        print("Recursively Found Prev: %i" % root.data)
+        return
+    if root.left is not None:
+        findPrevRec(root.left, n)
+    if root.right is not None:
+        findPrevRec(root.right, n)
+
 
 def findMinRec(root):
     """Recursive Find Min"""
@@ -46,6 +63,7 @@ def findMinRec(root):
     else:
         findMinRec(root.left)
 
+
 def findMaxRec(root):
     """Recursive Find Max"""
     if root.right is None:
@@ -54,31 +72,38 @@ def findMaxRec(root):
     else:
         findMaxRec(root.right)
 
+
 def insertIter(root, node):
     """Iterative Insertion"""
-    while node.data < root.data:
-        if root.left is None:
-            root.left = node
-            break
-        root = root.left
-    while node.data > root.data:
-        if root.right is None:
-            root.right = node
-            break
-        root = root.right
-        
+    while True:
+        if root.data < node.data:
+            if root.right is None:
+                root.right = node
+                return
+            else:
+                root = root.right
+        else:
+            if root.left is None:
+                root.left = node
+                return
+            else:
+                root = root.left
+
 
 def deleteIter(root, node):
     """Iterative Deletion"""
     pass
 
-def findNextIter(root, node):
+
+def findNextIter(root, n):
     """Iterative Find Next"""
     pass
+
 
 def findPrevIter(root, node):
     """Iterative Find Prev"""
     pass
+
 
 def findMinIter(root):
     """Iterative Find Min"""
@@ -86,51 +111,112 @@ def findMinIter(root):
         root = root.left
     print("Iteratively Found Min: %i" % root.data)
 
+
 def findMaxIter(root):
     """Iterative Find Max"""
     while root.right is not None:
         root = root.right
     print("Iteratively Found Max: %i" % root.data)
 
-def sort(root):
-    """Sort"""
-    pass
+
+def sort(arr):
+    """Sorting Function"""
+    tree = Node(arr[0])
+    for item in arr:
+        if item == tree.data:
+            continue
+        insertRec(tree, Node(item))
+    ret = []
+    return treeSort(tree, ret)
+
+
+def treeSort(root, ret):
+    """Helper Function Building Sorted Array Recursively"""
+    if root is None:
+        return
+    treeSort(root.left, ret)
+    ret.append(root.data)
+    treeSort(root.right, ret)
+    return ret
+
 
 def display(root):
-    """Prints All Nodes"""
+    """For My Testing Purposes: Prints All Nodes"""
     if root is None:
         return
     display(root.left)
     print(root.data)
     display(root.right)
-  
+
+
 def getRandomArray(n):
     """Create Randomly Filled Array"""
     temp = random.randint(0, 999)
-    arr = [None]*n
+    arr = [None] * n
     for i in range(n):
         while temp in arr:
             temp = random.randint(0, 999)
         arr[i] = temp
     return arr
-  
+
+
 def getSortedArray(n):
     """Created Sorted Array"""
-    arr = [None]*n
+    arr = [None] * n
     for i in range(n):
         arr[i] = n - i
     return arr
 
-root = Node(50)
-insertIter(root, Node(10))
-insertIter(root, Node(75))
-insertIter(root, Node(90))
-insertIter(root, Node(15))
-insertIter(root, Node(1))
-insertIter(root, Node(60))
-insertIter(root, Node(29))
-display(root)
-findMinRec(root)
-findMaxRec(root)
-#print(getRandomArray(10))
-#print(getSortedArray(10))
+print("Test Recursive Functions")
+rootRec = Node(50)
+insertRec(rootRec, Node(10))
+insertRec(rootRec, Node(75))
+insertRec(rootRec, Node(90))
+insertRec(rootRec, Node(15))
+insertRec(rootRec, Node(30))
+insertRec(rootRec, Node(66))
+insertRec(rootRec, Node(17))
+insertRec(rootRec, Node(400))
+insertRec(rootRec, Node(1))
+insertRec(rootRec, Node(60))
+insertRec(rootRec, Node(29))
+display(rootRec)
+findMinRec(rootRec)
+findMaxRec(rootRec)
+findNextRec(rootRec, 17)
+findPrevRec(rootRec, 75)
+deleteRec(rootRec, 400)
+deleteRec(rootRec, 60)
+deleteRec(rootRec, 50)
+display(rootRec)
+
+print("\nTest Iterative Functions")
+rootIter = Node(50)
+insertIter(rootIter, Node(10))
+insertIter(rootIter, Node(75))
+insertIter(rootIter, Node(90))
+insertIter(rootIter, Node(15))
+insertIter(rootIter, Node(30))
+insertIter(rootIter, Node(66))
+insertIter(rootIter, Node(17))
+insertIter(rootIter, Node(400))
+insertIter(rootIter, Node(1))
+insertIter(rootIter, Node(60))
+insertIter(rootIter, Node(29))
+display(rootIter)
+findMinIter(rootIter)
+findMaxIter(rootIter)
+findNextIter(rootIter, 17)
+findPrevIter(rootIter, 75)
+deleteIter(rootIter, 400)
+deleteIter(rootIter, 60)
+deleteIter(rootIter, 50)
+display(rootIter)
+
+#Test Question 2 & 3
+randomArr = getRandomArray(10)
+print(randomArr)
+sortedArr = sort(randomArr)
+print(sortedArr)
+
+print(getSortedArray(10))
